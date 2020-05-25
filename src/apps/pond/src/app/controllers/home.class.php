a4715e23e59f2b80e5bb0a7c65fafafd81a6f58b
david-blanchard <dpjb@hotmail.fr>
2020-05-25 20:35:20 +0200

    Update the web site

--- src/apps/pond/src/app/controllers/home.class.php
+++ src/apps/pond/src/app/controllers/home.class.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace CodePhoenixOrg\Pond\pond\Controllers;
 
 use CodePhoenixOrg\Pond\Framework\Web\Controller;
@@ -7,12 +8,10 @@ class Home extends Controller
 {
     public function load(): ?array
     {
-        return ["items" => [
-                ["title" => "CodePhoenixOrg.fr pages", "url" => "pages.html?language=fr&country=FR&partner=7884&partnerid=110&projectid=15"],
-                ["title" => "CodePhoenixOrg.fr products", "url" => "products.html?language=fr&country=FR&partner=7884&partnerid=110&projectid=15"],
-                ["title" => "CodePhoenixOrg.fr placeholders", "url" => "placeholders.html?language=fr&country=FR&partner=7884&partnerid=110&projectid=15"],
-                ["title" => "CodePhoenixOrg.fr noindex", "url" => "noindex.html"],
-                ["title" => "CodePhoenixOrg.fr token", "url" => "token.html"],
+        return [
+            "items" => [
+                ["title" => "CodePhoenixOrg.fr Hello", "url" => "hello.html"],
+                ["title" => "CodePhoenixOrg.fr Landing", "url" => "land.html"],
             ]
         ];
     }
