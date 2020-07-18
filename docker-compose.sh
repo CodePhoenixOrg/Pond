4892c5d9b0323d843d381b6b91ad5c915a31cace
David Blanchard <dpjb@hotmail.fr>
2020-07-18 18:20:52 +0000

    Move or remove tools

--- docker-compose.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker-compose -f "docker-compose.yml" up -d --build
-docker exec -t pond_php_1 /bin/sh -c "chmod 775 /usr/local/bin/hop; /usr/local/bin/hop"
--- docker-image.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker build --pull --rm -f "docker/Dockerfile" -t pond:latest "docker"
-
